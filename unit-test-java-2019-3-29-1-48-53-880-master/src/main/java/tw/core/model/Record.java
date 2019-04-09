2e9e34213ca8de054dc4d126bd585bf87f742d68
Chen Meng <94532161@qq.com>
2019-04-09 17:05:13 +0800

    [Dv Homework Unit Test] add 3 test cases

--- unit-test-java-2019-3-29-1-48-53-880-master/src/main/java/tw/core/model/Record.java
+++ unit-test-java-2019-3-29-1-48-53-880-master/src/main/java/tw/core/model/Record.java
@@ -19,4 +19,5 @@ public class Record {
     public void increaseIncludeOnlyNum() {
         this.value[1]++;
     }
+
 }
--- unit-test-java-2019-3-29-1-48-53-880-master/src/test/java/tw/core/AnswerTest.java
+++ unit-test-java-2019-3-29-1-48-53-880-master/src/test/java/tw/core/AnswerTest.java
@@ -22,16 +22,37 @@ public class AnswerTest {
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
 
+    //Verify the create Answer
     @Test
-    public void Is_InputNumberValid() throws Exception {
+    public void Check_Created_Answer() throws Exception{
         Answer answer = new Answer();
+        Answer answer2 = new Answer();
         List<String> inputList = new ArrayList<>();
-        inputList.add("1");
-        //inputList.add("a");
-        inputList.add("3");
-        inputList.add("4");
+        for (int i = 1; i < 5; i++)
+            inputList.add(i+"");
+        String inputString = "1 2 3 4";
+        answer2.setNumList(inputList);
+        answer = answer.createAnswer(inputString);
+        assertThat(answer.createAnswer(inputString)).isEqualsToByComparingFields(answer2);
+    }
+
+    //If the input string length is not legal
+    @Test
+    public void isValid() throws Exception {
+        Answer answer = new Answer();
+        Answer answer2 = new Answer();
+
+        List<String> inputList = new ArrayList<>();
+        for (int i = 1; i < 3; i++)
+            inputList.add(i+"");
+
         answer.setNumList(inputList);
-        System.out.println(inputList);
+
+        List<String> inputList2 = new ArrayList<>();
+        for (int i = 1; i < 6; i++)
+            inputList2.add(i+"");
+
+        answer2.setNumList(inputList2);
 
         try {
             answer.validate();
@@ -40,6 +61,26 @@ public class AnswerTest {
                     .hasMessage("Answer format is incorrect");
         }
 
+        try {
+            answer2.validate();
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(OutOfRangeAnswerException.class)
+                    .hasMessage("Answer format is incorrect");
+        }
+    }
+
+    //Check the index position
+    @Test
+    public void Check_Index_of_NumberList() throws Exception{
+        Answer answer = new Answer();
+
+        List<String> inputList = new ArrayList<>();
+        for (int i = 1; i < 5; i++)
+            inputList.add(i+"");
+
+        answer.setNumList(inputList);
+        assertThat(answer.getIndexOfNum("1")).isEqualTo(0);
+        assertThat(answer.getIndexOfNum("6")).isEqualTo(-1);
 
     }
 
